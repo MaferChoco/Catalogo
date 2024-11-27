ff734202c3fdea91cef98a3cd0b973f8f499d2ac
MaferChoco <e.vco.arnold.cabrera@cali.edu.co>
2024-11-26 20:36:11 -0500

    Add files via upload

---
+++ navidad.js
@@ -0,0 +1,29 @@
+const treeContainer = document.createElement("pre"); // Opcionalmente puedes eliminar esta línea si no usarás el árbol.
+const mainContainer = document.querySelector(".main-container");
+
+const getRandomValue = (max, min = 1) => {
+  return Math.floor(Math.random() * max) + min;
+};
+
+const createSnow = (density) => {
+  for (let i = 0; i < density; i++) {
+    const snowFlake = document.createElement("span");
+    const horizontalPosition = `${getRandomValue(100)}%`;
+    const fallDelay = `${getRandomValue(100)}s`;
+    const fallDuration = `${getRandomValue(20, 5)}s`;
+    const flakeSize = `${getRandomValue(7, 1)}px`;
+    const flakeOpacity = Math.random().toFixed(2);
+
+    snowFlake.classList.add("snow");
+    snowFlake.style.opacity = flakeOpacity;
+    snowFlake.style.width = flakeSize;
+    snowFlake.style.height = flakeSize;
+    snowFlake.style.animation = `fall ${fallDuration} ${fallDelay} linear infinite`;
+    snowFlake.style.right = horizontalPosition;
+
+    mainContainer.appendChild(snowFlake);
+  }
+};
+
+// createTree(12); // Eliminado o comentado para evitar que se genere el árbol.
+createSnow(300); // Mantén la nieve.
